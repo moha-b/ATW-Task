eb55b5132aa96ffcd605ec0ab64e0e7afa121c79
hab <mohab.khalid.mahmoud@gmail.com>
2023-07-07 03:06:35 +0300

    api service

---
+++ lib/core/services/api_service.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final baseUrl = BASE_URL;
+  final Dio _dio;
+
+  ApiService(this._dio);
+
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    var response = await _dio.get(baseUrl + endPoint);
+    return response.data;
+  }
+}
