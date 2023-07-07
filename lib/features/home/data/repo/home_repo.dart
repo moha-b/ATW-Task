ff9295fc4d49b9db05f7150b8336afb8b3a34a31
hab <mohab.khalid.mahmoud@gmail.com>
2023-07-07 03:32:02 +0300

    implement data source

--- lib/features/home/data/repo/home_repo.dart
+++ lib/features/home/data/repo/home_repo.dart
@@ -4,6 +4,6 @@ import 'package:task/features/home/data/model/home_models.dart';
 
 abstract class HomeRepository {
   Future<Either<Failure, UserModel>> fetchUserData();
-  Future<Either<Failure, AlbumModel>> fetchUserAlbums();
-  Future<Either<Failure, PhotoModel>> fetchAlbumsPhotos();
+  Future<Either<Failure, AlbumModel>> fetchUserAlbums(int userId);
+  Future<Either<Failure, PhotoModel>> fetchAlbumsPhotos(int albumId);
 }
--- lib/features/home/data/source/home_repo_impl.dart
+++ lib/features/home/data/source/home_repo_impl.dart
@@ -1,24 +1,41 @@
 import 'package:dartz/dartz.dart';
 import 'package:task/core/errors/failure.dart';
+import 'package:task/core/services/api_service.dart';
 import 'package:task/features/home/data/model/home_models.dart';
 import 'package:task/features/home/data/repo/home_repo.dart';
 
 class HomeRepoImpl extends HomeRepository {
+  ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+
   @override
-  Future<Either<Failure, PhotoModel>> fetchAlbumsPhotos() {
-    // TODO: implement fetchAlbumsPhotos
-    throw UnimplementedError();
+  Future<Either<Failure, UserModel>> fetchUserData() async {
+    try {
+      var data = await apiService.get(endPoint: '/users/2');
+      return right(UserModel.fromJson(data));
+    } catch (e) {
+      return Left(ServerFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, AlbumModel>> fetchUserAlbums() {
-    // TODO: implement fetchUserAlbums
-    throw UnimplementedError();
+  Future<Either<Failure, AlbumModel>> fetchUserAlbums(int userId) async {
+    try {
+      var data = await apiService.get(endPoint: '/users/$userId/albums');
+      return right(AlbumModel.fromJson(data));
+    } catch (e) {
+      return Left(ServerFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, UserModel>> fetchUserData() {
-    // TODO: implement fetchUserData
-    throw UnimplementedError();
+  Future<Either<Failure, PhotoModel>> fetchAlbumsPhotos(int albumId) async {
+    try {
+      var data = await apiService.get(endPoint: '/albums/$albumId/photos');
+      return right(PhotoModel.fromJson(data));
+    } catch (e) {
+      return Left(ServerFailure());
+    }
   }
 }
